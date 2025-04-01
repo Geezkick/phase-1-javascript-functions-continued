d4b9bc92f6d40f3cd448ce920e4fd613fc98bd58
Brian <nyarienyabrian05@gmail.com>
2025-04-01 13:01:17 +0300

    Implement saturdayFun, mondayWork, and wrapAdjective functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+// Function Declaration with default parameter
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function Expression with default parameter
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Function that returns a closure with visual flair
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
